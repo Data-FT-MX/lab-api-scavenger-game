29afdb98051b34f80d98dda29e63201371f1c04a
FranciscoEP <francisco.eugp@gmail.com>
2022-04-05 21:45:57 -0500

    Lab challenges - completed missing bonus

--- challenge-1.py
+++ challenge-1.py
@@ -1,1 +1,19 @@
 # enter your code below
+from github import Github
+
+lab = 'ta-data-mex/lab-api-scavenger-game'
+
+token = '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'
+
+github = Github(token)
+
+user = github.get_user()
+user.login
+print(user.login)
+
+
+repo = github.get_repo(lab)
+print(repo)
+
+print(list(repo.get_forks()))
+
